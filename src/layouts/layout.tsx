1c0ca9226f731c0e7de60db72bd4c14e11a3acf0
@SonFE <son.tran@usumsoftware.com>
2025-02-11 23:23:58 +0700

    done home page

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -2,17 +2,16 @@ import { Outlet } from "react-router-dom";
 import SideBar from "./sidebar";
 
 function Layout() {
-
-    return (
-        <div className="w-full no-scrollbar ">
-            <div className=" lg:ml-[100px] lg:p-10  3xl:p-20 3xl:pb-0 no-scrollbar">
-                <div className=" mx-auto lg:max-w-[1100px] 3xl:max-w-[1660px]   bg-background ">
-                    <Outlet />
-                </div>
-            </div>
-            <SideBar />
+  return (
+    <div className="no-scrollbar w-full">
+      <div className="no-scrollbar mb-[68px] p-5 lg:mb-0 lg:ml-[100px] lg:p-10 3xl:p-20 3xl:pb-0">
+        <div className="mx-auto bg-background lg:max-w-[1100px] 3xl:max-w-[1660px]">
+          <Outlet />
         </div>
-    )
+      </div>
+      <SideBar />
+    </div>
+  );
 }
 
 export default Layout;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -7,54 +7,83 @@ import { useResponsiveProps } from "@/hooks/responsive.hook";
 import Corner from "./components/corner";
 
 function Home() {
-  const isLargeScreen = useResponsiveProps({ lg: true });
+  const isLargeScreen = useResponsiveProps({ xl: true });
+  console.log(isLargeScreen);
   return (
-    <div className=" flex flex-col gap-5 3xl:gap-4">
-      <div className="flex flex-col gap-5 lg:gap-3 lg:flex-row 3xl:gap-5 justify-between">
-        <div className="flex ">
-          <div className="lg:max-w-[432px] 3xl:max-w-[613px] w-full">
-            <SectionWrapper wrapperClassName=" w-full lg:rounded-tr-none lg:h-[450px] 3xl:h-auto lg:pr-0 3xl:rounded-tr-none 3xl:pr-0 h-fit " divClassName="lg:p-[18px] lg:h-[450px] 3xl:h-auto" >
-              <div className="relative  pl-[38px] pr-[37px] w-full max-w-[326px] rounded-[20px] lg:max-w-[396px] lg:px-[43.5px] bg-gradient h-full 3xl:max-w-[557px] 3xl:px-[40.5px]">
-                <img src={DataHome.person.avatar} className="w-full aspect-[251/326] lg:aspect-[308/400] 3xl:aspect-[476/618]" alt="Hero Image" />
+    <div className="flex flex-col gap-5 3xl:gap-4">
+      <div className="flex flex-col justify-between gap-5 xl:flex-row xl:gap-3 3xl:gap-5">
+        <div className="flex">
+          <div className="w-full xl:max-w-[432px] 3xl:max-w-[613px]">
+            <SectionWrapper
+              wrapperClassName=" w-full xl:rounded-tr-none xl:h-[450px] 3xl:h-auto xl:pr-0 3xl:rounded-tr-none 3xl:pr-0 h-fit "
+              divClassName="xl:p-[18px] xl:h-[450px] 3xl:h-auto md:flex gap-3"
+            >
+              <div className="bg-gradient relative mx-auto h-full w-full max-w-[326px] shrink-0 rounded-[20px] pl-[38px] pr-[37px] xl:max-w-[396px] xl:px-[43.5px] 3xl:max-w-[557px] 3xl:px-[40.5px]">
+                <img
+                  src={DataHome.person.avatar}
+                  className="aspect-[251/326] w-full xl:aspect-[308/400] 3xl:aspect-[476/618]"
+                  alt="Hero Image"
+                />
               </div>
-              {
-                !isLargeScreen && (
-                  <>
-                    <h1 className="text-20b 3xl:text-32b  mt-6 3xl:mt-0 font-bold text-gravel-25">{DataHome.person.name}</h1>
-                    <h2 className="text-14s 3xl:text-18s mt-2 font-semibold text-gravel-300">{DataHome.person.position}</h2>
-                    <p className="text-14r 3xl:text-20r mt-4 text-gravel-100">{DataHome.person.description}</p>
-                    <div className="bg-gradient-2 text-base mt-6 grid h-[52px] place-items-center rounded-[12px] text-16s font-semibold 3xl:bg-none 3xl:text-32b">
-                      <p className="3xl:text-gradient">My resume</p>
-                    </div>
-                  </>
-                )
-              }
+              {!isLargeScreen && (
+                <div className="md:flex md:flex-col md:gap-4">
+                  <h1 className="mt-6 text-20b font-bold text-gravel-25 3xl:mt-0 3xl:text-32b">
+                    {DataHome.person.name}
+                  </h1>
+                  <h2 className="mt-2 text-14s font-semibold text-gravel-300 3xl:text-18s">
+                    {DataHome.person.position}
+                  </h2>
+                  <p className="mt-4 text-14r text-gravel-100 3xl:text-20r">
+                    {DataHome.person.description}
+                  </p>
+                  <div className="bg-gradient-2 text-base mt-6 grid h-[52px] place-items-center rounded-[12px] text-16s font-semibold 3xl:bg-none 3xl:text-32b">
+                    <p className="3xl:text-gradient">My resume</p>
+                  </div>
+                </div>
+              )}
             </SectionWrapper>
-
           </div>
-          <div className={cn("flex flex-col  gap-4 lg:gap-3 3xl:gap-4", {
-            'hidden': !isLargeScreen
-          })}>
-            <SectionWrapper wrapperClassName={cn("overflow-visible lg:rounded-tl-none lg:rounded-bl-none lg:max-w-[276px] 3xl:rounded-tl-none 3xl:rounded-bl-none 3xl:max-w-[425px]  lg:bg-none lg:bg-block-bg lg:pl-0 3xl:pl-0 relative")} divClassName="lg:rounded-tl-none lg:rounded-bl-none  lg:bg-none lg:p-[19px] 3xl:px-[27px] 3xl:rounded-tl-none 3xl:rounded-bl-none">
-              <h1 className="text-20b 3xl:text-32b  mt-6 font-bold lg:mt-0 text-gravel-25">{DataHome.person.name}</h1>
-              <h2 className="text-14s 3xl:text-18s mt-2 font-semibold text-gravel-300">{DataHome.person.position}</h2>
-              <p className="text-14r lg:text-12r 3xl:text-20r mt-4 lg:mb-[38px] 3xl:mb-0 3xl:mt-6 text-gravel-100 max-w-[369px] w-full">{DataHome.person.description}</p>
+          <div
+            className={cn("flex flex-col gap-4 xl:gap-3 3xl:gap-4", {
+              hidden: !isLargeScreen,
+            })}
+          >
+            <SectionWrapper
+              wrapperClassName={cn(
+                "overflow-visible xl:rounded-tl-none xl:rounded-bl-none xl:max-w-[276px] 3xl:rounded-tl-none 3xl:rounded-bl-none 3xl:max-w-[425px]  xl:bg-none xl:bg-block-bg xl:pl-0 3xl:pl-0 relative",
+              )}
+              divClassName="xl:rounded-tl-none xl:rounded-bl-none  xl:bg-none xl:p-[19px] 3xl:px-[27px] 3xl:rounded-tl-none 3xl:rounded-bl-none"
+            >
+              <h1 className="mt-6 text-20b font-bold text-gravel-25 xl:mt-0 3xl:text-32b">
+                {DataHome.person.name}
+              </h1>
+              <h2 className="mt-2 text-14s font-semibold text-gravel-300 3xl:text-18s">
+                {DataHome.person.position}
+              </h2>
+              <p className="mt-4 w-full max-w-[369px] text-14r text-gravel-100 xl:mb-[38px] xl:text-12r 3xl:mb-0 3xl:mt-6 3xl:text-20r">
+                {DataHome.person.description}
+              </p>
               <Corner />
             </SectionWrapper>
             <Link to={DataHome.resume.href} target="_blank">
-              <SectionWrapper wrapperClassName="ml-4  lg:h-[126px] 3xl:h-[191px]" divClassName=" lg:h-[126px] 3xl:h-[191px] flex flex-col items-center justify-center">
-                <div className="bg-gradient-2 text-base  grid  place-items-center rounded-[12px] text-16s font-semibold lg:bg-none lg:text-20b 3xl:text-32b">
-                  <p className="lg:text-gradient">{DataHome.resume.text}</p>
+              <SectionWrapper
+                wrapperClassName="ml-4  xl:h-[126px] 3xl:h-[191px]"
+                divClassName=" xl:h-[126px] 3xl:h-[191px] flex flex-col items-center justify-center"
+              >
+                <div className="bg-gradient-2 text-base grid place-items-center rounded-[12px] text-16s font-semibold xl:bg-none xl:text-20b 3xl:text-32b">
+                  <p className="xl:text-gradient">{DataHome.resume.text}</p>
                 </div>
               </SectionWrapper>
             </Link>
-
           </div>
         </div>
 
-        <SectionWrapper wrapperClassName="lg:max-w-[380px] 3xl:h-[674px] 3xl:max-w-[600px] w-full h-fit" divClassName="3xl:h-[672px]">
+        <SectionWrapper
+          wrapperClassName="xl:max-w-[380px] 3xl:h-[674px] 3xl:max-w-[600px] w-full h-fit"
+          divClassName="3xl:h-[672px]"
+        >
           <HomePageTitle value={DataHome.experience.name} />
-          <div className="mt-6 3xl:mt-7 flex flex-col justify-between gap-y-3 lg:gap-y-[6.5px]">
+          <div className="mt-6 flex flex-col justify-between gap-y-3 xl:gap-y-[6.5px] 3xl:mt-7">
             {DataHome.experience.data.map((item, index) => (
               <div
                 key={index}
@@ -65,7 +94,7 @@ function Home() {
                 <div className="relative size-[41px] shrink-0 grow-0 3xl:size-[65px]">
                   <img src={item.icon} alt="Company Logo" />
                 </div>
-                <div className="text-xs flex flex-1 flex-col justify-between gap-2 lg:gap-1">
+                <div className="text-xs flex flex-1 flex-col justify-between gap-2 xl:gap-1">
                   <div className="flex justify-between gap-x-2 text-12s text-gravel-25 3xl:text-20s">
                     {item.name.includes("-") ? (
                       <div className="flex gap-1 3xl:gap-[15px]">
@@ -78,9 +107,9 @@ function Home() {
                     ) : (
                       <p>{item.name}</p>
                     )}
-                    <p className=" 3xl:text-18m">{item.role}</p>
+                    <p className="3xl:text-18m">{item.role}</p>
                   </div>
-                  <div className="text-12r lg:text-10r 3xl:text-16r mt-auto flex justify-between gap-x-2 text-gravel-300">
+                  <div className="mt-auto flex justify-between gap-x-2 text-12r text-gravel-300 xl:text-10r 3xl:text-16r">
                     <div>{item.date}</div>
                     {!!item.mode && <div className="">{item.mode}</div>}
                   </div>
@@ -90,74 +119,61 @@ function Home() {
           </div>
         </SectionWrapper>
       </div>
-      <div className="flex flex-col gap-5 lg:flex-row 3xl:gap-4 flex-1">
-        <SectionWrapper wrapperClassName="h-fit">
-          <div className="flex flex-col gap-4 3xl:flex-row 3xl:gap-7">
-            <div className="flex flex-col gap-6 3xl:gap-[6px]">
+      <div className="flex flex-1 flex-col gap-5 xl:flex-row xl:gap-3 3xl:gap-4">
+        <SectionWrapper wrapperClassName="h-fit" divClassName={"xl:py-[18px] 3xl:py-7"}>
+          <div className="flex flex-col gap-4 xl:flex-row 3xl:gap-7">
+            <div className="flex flex-col gap-6 xl:gap-4 3xl:gap-[6px]">
               <HomePageTitle value={DataHome.toolIUse.text} />
-              <div className=" text-gravel-100 text-14m 3xl:text-20m max-w-[343px]">
+              <div className="text-14r text-gravel-100 xl:max-w-[194px] xl:text-12r 3xl:max-w-[343px] 3xl:text-20r">
                 {DataHome.toolIUse.content}
               </div>
             </div>
-            <div className=" flex items-center gap-x-4 3xl:gap-x-3">
-              <div className="size-[60px] 3xl:size-[174px] 3xl:rounded-[10.5px] bg-background grid place-items-center rounded-[8px]">
-                <img
-                  src={DataHome.toolIUse.icon.figma}
-                  alt="Figma"
-                />
+            <div className="flex items-center gap-x-4 3xl:gap-x-3">
+              <div className="grid size-[60px] place-items-center rounded-[8px] bg-background xl:size-[138px] 3xl:size-[174px] 3xl:rounded-[10.5px]">
+                <img src={DataHome.toolIUse.icon.figma} alt="Figma" />
                 {/* <Figma size={30} /> */}
               </div>
-              <div className=" flex items-center gap-x-4 3xl:flex-col gap-y-3">
-                <div className="size-[60px] 3xl:size-[81px]  bg-background  3xl:rounded-[10.5px] bg-0f grid place-items-center rounded-[8px]">
-                  <div className="relative w-8 h-[23.93px]">
-                    <img
-                      src={DataHome.toolIUse.icon.ps}
-                      alt="Photoshop"
-                    />
+              <div className="flex items-center gap-x-4 gap-y-3 xl:flex-col">
+                <div className="bg-0f grid size-[60px] place-items-center rounded-[8px] bg-background xl:size-[65px] 3xl:size-[81px] 3xl:rounded-[10.5px]">
+                  <div className="relative h-[23.93px] w-8">
+                    <img src={DataHome.toolIUse.icon.ps} alt="Photoshop" />
                   </div>
                 </div>
-                <div className="size-[60px] bg-background 3xl:size-[81px] 3xl:rounded-[10.5px] grid place-items-center rounded-[8px]">
-                  <div className="relative w-8 h-[28.02px]">
-                    <img
-                      src={DataHome.toolIUse.icon.ai}
-                      alt="AI"
-                    />
+                <div className="grid size-[60px] place-items-center rounded-[8px] bg-background xl:size-[65px] 3xl:size-[81px] 3xl:rounded-[10.5px]">
+                  <div className="relative h-[28.02px] w-8">
+                    <img src={DataHome.toolIUse.icon.ai} alt="AI" />
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
         </SectionWrapper>
 
-        <SectionWrapper wrapperClassName="h-fit 3xl:max-w-[950px] w-full">
+        <SectionWrapper wrapperClassName="h-fit xl:max-w-[623px] 3xl:max-w-[950px] w-full">
           <HomePageTitle value={DataHome.contact.text} />
-          <div className="mt-6 3xl:mt-7 grid grid-cols-2 gap-4 flex-1 3xl:flex  3xl:items-center 3xl:gap-[14.6px]">
+          <div className="mt-6 grid flex-1 grid-cols-2 gap-4 xl:mt-4 xl:flex xl:items-center 3xl:mt-7 3xl:gap-[14.6px]">
             {DataHome.contact.socials.map((item, index) => (
               <Link
-                to={item.href} target="_blank"
-                className={cn('rounded-[12px] py-[17px] w-full 3xl:rounded-[14.6px] bg-gravel-800 flex flex-col justify-center items-center gap-2 transition-colors  ',
+                to={item.href}
+                target="_blank"
+                className={cn(
+                  "flex w-full flex-col items-center justify-center gap-2 rounded-[12px] bg-gravel-800 py-[17px] transition-colors xl:py-[19.5px] 3xl:rounded-[14.6px] 3xl:py-[17px]",
                   {
-                    'hover:bg-likedin': item.name === 'LinkedIn',
-                    'hover:bg-gmail': item.name === 'Email',
-                    'hover:bg-behance': item.name === 'Behance',
-                    'hover:bg-dribbble': item.name === 'Dribbble',
-                  }
+                    "hover:bg-likedin": item.name === "LinkedIn",
+                    "hover:bg-gmail": item.name === "Email",
+                    "hover:bg-behance": item.name === "Behance",
+                    "hover:bg-dribbble": item.name === "Dribbble",
+                  },
                 )}
                 key={index}
               >
-                <img
-                  src={item.icon}
-                  alt="Linkedin"
-                  className="size-6 3xl:size-[30px]"
-                />
-                <div className="text-14m 3xl:text-18m text-gravel-25 ">{item.name}</div>
+                <img src={item.icon} alt="Linkedin" className="size-6 3xl:size-[30px]" />
+                <div className="text-14m text-gravel-25 3xl:text-18m">{item.name}</div>
               </Link>
             ))}
           </div>
         </SectionWrapper>
       </div>
-
     </div>
   );
 }
