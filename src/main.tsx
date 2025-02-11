06d62a871860a14459978d022139c7a30b6f290c
@SonFE <son.tran@usumsoftware.com>
2025-02-11 23:49:16 +0700

    init project-highlight page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import router from './routes'
-import { RouterProvider } from 'react-router-dom'
-import ResponsiveProvider from './hooks/responsive.provider'
-
-createRoot(document.getElementById('root')!).render(
-    <ResponsiveProvider>
-        <RouterProvider router={router} />
-    </ResponsiveProvider>
-)
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import router from "./routes";
+import { RouterProvider } from "react-router-dom";
+import ResponsiveProvider from "./hooks/responsive.provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+createRoot(document.getElementById("root")!).render(
+  <ResponsiveProvider>
+    <RouterProvider router={router} />
+    <SpeedInsights />
+  </ResponsiveProvider>,
+);
