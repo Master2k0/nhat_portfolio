e44cdeccd9367cfa9eed561b7e196f52f6ab6a7b
Tran Hoang Son [sontran2x] <sontran2x@nexondv.com>
2025-02-12 12:45:14 +0700

    Update home to center

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./sidebar";
 function Layout() {
-  return (
-    <div className="no-scrollbar w-full">
-      <div className="no-scrollbar mb-[68px] p-5 lg:mb-0 lg:ml-[100px] lg:p-10 3xl:p-20 3xl:pb-0">
-        <div className="mx-auto bg-background lg:max-w-[1100px] 3xl:max-w-[1660px]">
-          <Outlet />
+    return (
+        <div className="no-scrollbar w-full">
+            <div className="no-scrollbar mb-[68px] p-5 lg:h-screen lg:mb-0 lg:ml-[100px] lg:p-10 3xl:p-20  ">
+                <div className="mx-auto bg-background w-full lg:max-w-[1100px] 3xl:max-w-[1660px] lg:h-[100%] lg:flex lg:flex-col lg:justify-center">
+                    <Outlet />
+                </div>
+            </div>
+            <SideBar />
         </div>
-      </div>
-      <SideBar />
-    </div>
-  );
+    );
 }
 
 export default Layout;
