f9302a0c3095e461f2029d201c7a527c09dd5c36
Tran Hoang Son [sontran2x] <18521351@gm.uit.edu.vn>
2025-09-04 15:36:50 +0700

    Add speed insights vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import { RouterProvider } from "react-router-dom";
 import ResponsiveProvider from "./hooks/responsive.provider";
 import { GlobalContext } from "./context/global-context";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById("root")!).render(
   <ResponsiveProvider>
     <Analytics />
+    <SpeedInsights />
     <GlobalContext>
       <RouterProvider router={router} />
     </GlobalContext>
