22ec84317cca7eb476f321c9ead1a47e5e7e1397
Tran Hoang Son [sontran2x] <18521351@gm.uit.edu.vn>
2025-12-10 10:19:16 +0700

    feat: 100% page vng

--- src/pages/projects/vng/vng.tsx
+++ src/pages/projects/vng/vng.tsx
@@ -240,202 +240,90 @@ function VNG() {
               <Content content={Datas.main.section3.hmw} className="text-gravel-25 mt-2" />
             </div>
           </SectionWrapper>
-          {/* <SectionWrapper
-            wrapperClassName="section-border overflow-hidden p-px  rounded-[20px] lg:rounded-[16px] 3xl:rounded-[28px] xl:rounded-[8px] 3xl:rounded-[8px]"
-            divClassName="rounded-[20px] bg-block-bg px-4 py-5 lg:rounded-[16px] 3xl:rounded-[28px] 3xl:p-7 xl:rounded-[8px] 3xl:rounded-[8px] xl:!p-[31px] "
-            id="about-logo"
-          >
-            <div className="flex flex-col">
-              <Header title={Datas.main.section2.title} />
-              <Divider />
-              <div className="flex flex-col gap-4 lg:gap-10">
-                {Datas.main.section2.content.map((content, index) => (
-                  <Content key={index} content={content} />
-                ))}
-              </div>
-            </div>
-            <img
-              alt=""
-              src={Datas.main.section2.img}
-              className="mt-6 w-full rounded-[8px] lg:mt-10 xl:rounded-[12px]"
-            />
-          </SectionWrapper>
           <SectionWrapper
             wrapperClassName="section-border overflow-hidden p-px  rounded-[20px] lg:rounded-[16px] 3xl:rounded-[28px] xl:rounded-[8px] 3xl:rounded-[8px]"
             divClassName="rounded-[20px] bg-block-bg px-4 py-5 lg:rounded-[16px] 3xl:rounded-[28px] 3xl:p-7 xl:rounded-[8px] 3xl:rounded-[8px] xl:!p-[31px] "
-            id="style-guide"
-          >
-            <div className="flex flex-col">
-              <Header title={Datas.main.section3.title} />
-              <Divider />
-              <div className="flex flex-col gap-5 lg:gap-8">
-                <p className="text-16s text-gravel-25 lg:text-24s">
-                  {Datas.main.section3.subSection1.title}
-                </p>
-                <div className="flex flex-col gap-4 lg:gap-5">
-                  {Datas.main.section3.subSection1.content.map((content, index) => (
-                    <Content key={index} content={content} />
-                  ))}
-                </div>
-                <img
-                  alt=""
-                  src={Datas.main.section3.subSection1.img1}
-                  className="w-full rounded-[8px] xl:rounded-[12px]"
-                />
-                <Content content={Datas.main.section3.subSection1.subContent} />
-                <img
-                  alt=""
-                  src={Datas.main.section3.subSection1.img2}
-                  className="w-full rounded-[8px] xl:rounded-[12px]"
-                />
-              </div>
-              <div className="border-dash-custom my-6 w-full lg:my-10" />
-              <div className="flex flex-col gap-5 lg:gap-8">
-                <div className="flex gap-3">
-                  <p className="font-montserrat text-16s text-gravel-25 lg:text-24s">
-                    {Datas.main.section3.subSection2.title[0]}
-                  </p>
-                  <p className="text-yellow-gradient font-montserrat text-16s lg:text-24s">
-                    {Datas.main.section3.subSection2.title[1]}
-                  </p>
-                </div>
-                <div className="flex flex-col gap-3 lg:flex-row  lg:items-center lg:gap-10">
-                  <p className="font-montserrat text-[133px] font-bold text-gravel-25">Aa</p>
-                  <div className="flex flex-col gap-4 xl:gap-5">
-                    <div className="flex gap-6 lg:gap-10">
-                      <p className="font-montserrat text-16r text-gravel-25 lg:text-18r">Regular</p>
-                      <p className="font-montserrat text-16m text-gravel-25 lg:text-18m">Medium</p>
-                      <p className="font-montserrat text-16s text-gravel-25 lg:text-18s">
-                        Semi Bold
-                      </p>
-                      <p className="font-montserrat text-16b text-gravel-25 lg:text-18b">Bold</p>
-                    </div>
-                    <p className="font-montserrat text-gravel-100">
-                      {Datas.main.section3.subSection2.content}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </SectionWrapper>
-          <SectionWrapper
-            wrapperClassName="section-border overflow-hidden p-px  rounded-[20px] lg:rounded-[16px] 3xl:rounded-[28px] xl:rounded-[8px] 3xl:rounded-[8px]"
-            divClassName="rounded-[20px] bg-block-bg px-4 py-5 lg:rounded-[16px] 3xl:rounded-[28px] 3xl:p-7 xl:rounded-[8px] 3xl:rounded-[8px] xl:!p-[31px] "
-            id="sitemap"
+            id="ideation"
           >
             <div className="flex flex-col">
               <Header title={Datas.main.section4.title} />
               <Divider />
-
-              <Content content={Datas.main.section4.content} />
+              <div className="flex flex-col gap-4 xl:gap-5">
+                <Content content={Datas.main.section4.content} />
+              </div>
             </div>
             <img
               alt=""
               src={Datas.main.section4.img}
-              className="mt-6 w-full rounded-[8px] lg:mt-10 xl:rounded-[12px]"
+              className="mt-6 w-full xl:mt-8 "
             />
+            <div className="flex flex-col gap-8">
+              <Content content={Datas.main.section4.belowContent} />
+              {Datas.main.section4.subContents.map((subContent, index) => (
+                <div key={index} className="flex flex-col xl:flex-row gap-4 xl:gap-5">
+                  <div className="flex flex-col gap-3">
+                    <Header title={subContent.index} />
+                    {subContent.contents.map((content, idx) => (
+                      <Content content={content} key={idx} />
+                    ))}
+                  </div>
+                  <img
+                    alt=""
+                    src={subContent.img}
+                    className="shrink-0 grow-0  max-w-[345px] "
+                  />
+                </div>
+              ))}
+            </div>
           </SectionWrapper>
           <SectionWrapper
             wrapperClassName="section-border overflow-hidden p-px  rounded-[20px] lg:rounded-[16px] 3xl:rounded-[28px] xl:rounded-[8px] 3xl:rounded-[8px]"
             divClassName="rounded-[20px] bg-block-bg px-4 py-5 lg:rounded-[16px] 3xl:rounded-[28px] 3xl:p-7 xl:rounded-[8px] 3xl:rounded-[8px] xl:!p-[31px] "
-            id="user-flow"
+            id="concept-validation"
           >
             <div className="flex flex-col">
               <Header title={Datas.main.section5.title} />
               <Divider />
-
-              <Content content={Datas.main.section5.content} />
+              <div className="flex flex-col gap-4 xl:gap-5">
+                <p
+                  className="text-14r text-gravel-100 xl:text-16r"
+                  dangerouslySetInnerHTML={{
+                    __html: Datas.main.section5.content,
+                  }}
+                />
+              </div>
             </div>
-            <img
-              alt=""
-              src={Datas.main.section5.img}
-              className="mt-6 w-full rounded-[8px] lg:mt-10 xl:rounded-[12px]"
-            />
+            <div className="flex flex-col gap-5 mt-5">
+              <div className="flex flex-col  xl:flex-row gap-5 w-full">
+                {Datas.main.section5.imgs.map((img, index) => (
+                  <img key={index} src={img} className="rounded-[8px] max-w-[223px]" />
+                ))}
+
+              </div>
+              {Datas.main.section5.imgs2.map((img, index) => (
+                <img key={index} src={img} className="rounded-[8px] w-full" />
+              ))}
+            </div>
           </SectionWrapper>
           <SectionWrapper
             wrapperClassName="section-border overflow-hidden p-px  rounded-[20px] lg:rounded-[16px] 3xl:rounded-[28px] xl:rounded-[8px] 3xl:rounded-[8px]"
             divClassName="rounded-[20px] bg-block-bg px-4 py-5 lg:rounded-[16px] 3xl:rounded-[28px] 3xl:p-7 xl:rounded-[8px] 3xl:rounded-[8px] xl:!p-[31px] "
-            id="wireframe"
+            id="final-design"
           >
             <div className="flex flex-col">
               <Header title={Datas.main.section6.title} />
               <Divider />
-              <Content content={Datas.main.section6.content} />
-            </div>
-            <div className="border-dash-custom invisible my-3 lg:visible lg:my-10" />
-            <div className="flex flex-col gap-5 lg:gap-8">
-              <p className="text-16s text-gravel-25 lg:text-24s">
-                {Datas.main.section6.mobile.title}
-              </p>
-              <div className="columns-3 gap-[13px] lg:columns-5 lg:gap-8">
-                {Datas.main.section6.mobile.imgs.map((img, index) => (
-                  <img
-                    key={index}
-                    src={img}
-                    alt={Datas.main.section6.mobile.title}
-                    className="mb-3 w-full lg:mb-8"
-                  />
-                ))}
+              <div className="flex flex-col gap-4 xl:gap-5">
+                <Content content={Datas.main.section6.content} />
               </div>
             </div>
-            <div className="border-dash-custom my-6 lg:my-10" />
-            <div className="flex flex-col gap-5 lg:gap-8">
-              <p className="text-16s text-gravel-25 lg:text-24s">{Datas.main.section6.pc.title}</p>
-              <div className="columns-2 gap-[13px] lg:columns-4 lg:gap-8">
-                {Datas.main.section6.pc.imgs.map((img, index) => (
-                  <img
-                    key={index}
-                    src={img}
-                    alt={Datas.main.section6.pc.title}
-                    className="mb-3 w-full lg:mb-4"
-                  />
-                ))}
-              </div>
+            <div className="flex flex-col gap-8 mt-8">
+
+              {Datas.main.section6.imgs.map((img, index) => (
+                <img key={index} src={img} className=" w-full" />
+              ))}
             </div>
           </SectionWrapper>
-          <SectionWrapper
-            wrapperClassName="section-border overflow-hidden p-px  rounded-[20px] lg:rounded-[16px] 3xl:rounded-[28px] xl:rounded-[8px] 3xl:rounded-[8px]"
-            divClassName="rounded-[20px] bg-block-bg px-4 py-5 lg:rounded-[16px] 3xl:rounded-[28px] 3xl:p-7 xl:rounded-[8px] 3xl:rounded-[8px] xl:!p-[31px] "
-            id="final-ui"
-          >
-            <div className="flex flex-col">
-              <Header title={Datas.main.section7.title} />
-              <Divider />
-              <div className="flex flex-col gap-4 lg:gap-5">
-                {Datas.main.section7.content.map((content, index) => (
-                  <Content content={content} key={index} />
-                ))}
-              </div>
-            </div>
-            <div className="border-dash-custom invisible my-3 lg:visible lg:my-10" />
-            <div className="flex flex-col gap-8">
-              <p className="text-16s text-gravel-25 lg:text-24s">Mobile Web</p>
-              <div className="flex flex-wrap gap-3 lg:gap-[36px]">
-                {Datas.main.section7.mobileImg.map((img, index) => (
-                  <img
-                    key={index}
-                    alt=""
-                    src={img}
-                    className="aspect-[1/2] w-[100px] rounded-[8px] lg:w-[150px] xl:rounded-[12px]"
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="border-dash-custom my-6 lg:my-10" />
-            <div className="flex flex-col gap-8">
-              <p className="text-16s text-gravel-25 lg:text-24s">Web Desktop</p>
-              <div className="columns-2 gap-3 lg:gap-[36px]">
-                {Datas.main.section7.pcImg.map((img, index) => (
-                  <img
-                    key={index}
-                    alt=""
-                    src={img}
-                    className="mb-6 w-full rounded-[8px] lg:mb-10 xl:rounded-[12px]"
-                  />
-                ))}
-              </div>
-            </div>
-          </SectionWrapper> */}
         </div>
       </div >
     </div >
